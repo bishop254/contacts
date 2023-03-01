ae776f78608802af5fd0565aa4013efd4d6c22f8
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-03-01 19:59:34 +0300

    Added a route guard.

---
+++ src/app/services/fireguard.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { FirebaseService } from './firebase.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FireguardGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: FirebaseService,
+    private snackBar: MatSnackBar
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authService.isLoggedIn !== true) {
+      this.router.navigate(['sign-in']);
+      this.snackBar.open(
+        'You are not authorized to view this page',
+        'Auth Error',
+        { duration: 2 * 1000 }
+      );
+    }
+    return true;
+  }
+}
