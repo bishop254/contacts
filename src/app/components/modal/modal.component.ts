c44cb57d0ec8423e5c737b4ad0483bb128d685ff
Karanja.Chiuri <karanjachiuri@gmail.com>
2023-03-01 23:46:45 +0300

    Cleaned up logic for the add/edit modal component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FirebaseService } from 'src/app/services/firebase.service';
 
@@ -9,19 +9,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
-  addressForm = this.fb.group({
-    firstname: [{value: this.data ? this.data['dialogData']['firstname' as keyof Object] : null}, Validators.required],
-    lastname: [{value: this.data ? this.data['dialogData']['lastname' as keyof Object] : null}, Validators.required],
-    email: [{value: this.data ? this.data['dialogData']['email' as keyof Object] : null}, [Validators.required, Validators.email]],
-    mobile: [
-      {value: this.data ? this.data['dialogData']['mobile' as keyof Object] : null},
-      [
-        Validators.required,
-        Validators.minLength(10),
-        Validators.pattern(/^(07\d{8}|011\d{7})$/),
-      ],
-    ],
-  });
+  addressForm!: FormGroup;
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
@@ -29,21 +17,58 @@ export class ModalComponent implements OnInit {
     private dialogRef: MatDialogRef<ModalComponent>,
     private fb: FormBuilder,
     private firebaseService: FirebaseService
-  ) {}
+  ) {
+    this.addressForm = this.fb.group({
+      firstname: [
+        this.data['dialogData'] !== undefined
+          ? this.data['dialogData']['firstname' as keyof Object]
+          : '',
+        Validators.required,
+      ],
+      lastname: [
+        this.data['dialogData']
+          ? this.data['dialogData']['lastname' as keyof Object]
+          : null,
+        Validators.required,
+      ],
+      email: [
+        this.data['dialogData']
+          ? this.data['dialogData']['email' as keyof Object]
+          : null,
+        [Validators.required, Validators.email],
+      ],
+      mobile: [
+        this.data['dialogData']
+          ? this.data['dialogData']['mobile' as keyof Object]
+          : null,
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.pattern(/^(07\d{8}|011\d{7})$/),
+        ],
+      ],
+    });
+  }
 
   ngOnInit(): void {
-    console.log(this.data);
-    
+    console.log(this.data['dialogData']);
   }
 
   onSubmit(): void {
-    let fname: string = this.addressForm.controls['firstname'].value!.toString();
+    let fname: string =
+      this.addressForm.controls['firstname'].value!.toString();
     let lname: string = this.addressForm.controls['lastname'].value!.toString();
     let em: string = this.addressForm.controls['email'].value!.toString();
     let mobile: string = this.addressForm.controls['mobile'].value!.toString();
     console.log(em, fname, lname, mobile);
-    console.log(this.addressForm.controls);
-    this.firebaseService.saveContact(fname, lname, em, mobile);
+    if (this.data['dialogTitle'] == 'Add Contact') {
+      this.firebaseService.saveContact(fname, lname, em, mobile);
+    } else {
+      let oldMobile =
+        this.data['dialogData']['mobile' as keyof Object].toString();
+      this.firebaseService.updateContact(fname, lname, em, mobile, oldMobile);
+      console.log(this.addressForm.controls, oldMobile);
+    }
 
     this.dialogRef.close();
   }
