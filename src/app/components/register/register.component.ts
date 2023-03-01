8ecf1ccc673039cf523cfab338560722f786946d
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-03-01 19:59:17 +0300

    Added the register component.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  addressForm = this.fb.group(
+    {
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      confirmPassword: [null, Validators.required],
+    },
+    {
+      validator: this.ConfirmedValidator('password', 'confirmPassword'),
+    }
+  );
 
-  constructor() { }
+  hasUnitNumber = false;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private auth: FirebaseService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    let em: string = this.addressForm.controls['email'].value!;
+    let pass: string = this.addressForm.controls['password'].value!;
+    this.auth.SignUp(em, pass);
   }
 
+  ConfirmedValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors['confirmedValidator']
+      ) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmedValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
 }
